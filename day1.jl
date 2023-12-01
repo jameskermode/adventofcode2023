acaf8fda1c3a4e4675f4585dbf288b656d988461
James Kermode <J.R.Kermode@warwick.ac.uk>
2023-12-01 13:10:37 +0000

    clean up

--- day1.jl
+++ day1.jl
@@ -13,7 +13,7 @@ function extract_calibration(input; words=false)
     return parse(Int, digits)
 end
 
-@testset "Problem 1" begin
+@testset "test 1" begin
 
     sample_inputs = ["1abc2",
     "pqr3stu8vwx",
@@ -22,7 +22,6 @@ end
 
     sample_outputs = [12, 38, 15, 77]
 
-
     outputs = [ extract_calibration(input) for input in sample_inputs ]
     @show outputs
     @show outputs .== sample_outputs
@@ -30,7 +29,7 @@ end
     @test sum(outputs) == sum(sample_outputs)
 end
 
-@testset "Problem 2" begin
+@testset "Test 2" begin
 
     sample_inputs = ["two1nine",
                     "eightwothree",
@@ -39,7 +38,7 @@ end
                     "4nineeightseven2",
                     "zoneight234",
                     "7pqrstsixteen",
-                    "sevenine"]
+                    "sevenine"] # tricky edge case with overlapping words
 
     sample_outputs = [29, 83, 13, 24, 42, 14, 76, 79]
 
@@ -54,9 +53,13 @@ println("My results:")
 mylines = open("input1.txt") do f
     readlines(f)
 end
-prob1 = sum([extract_calibration(line) for line in mylines])
-prob2 = sum([extract_calibration(line; words=true) for line in mylines])
 
-@show prob1
-@show prob2
+@testset "Problems" begin
+    prob1 = sum([extract_calibration(line) for line in mylines])
+    @show prob1
+    @test prob1 == 53194
 
+    prob2 = sum([extract_calibration(line; words=true) for line in mylines])
+    @show prob2
+    @test prob2 == 54249
+end
